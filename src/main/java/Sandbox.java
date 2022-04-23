7f39e9db1783a986942e529f8ec4bfefa3a639b8
pablo <pmkn83@gmail.com>
2022-04-23 01:13:50 -0300

    Part 01 Exercise 01 Sandbox

---
+++ src/main/java/Sandbox.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Write your program here
+        double pi = 3.14;
+        pi = 3.141592653;
+
+        System.out.println("The value of pi is: " + pi);
+    }
+}
